280c206a1a6cec7328eafa32cd73ff3c1a7a634d
leticiarodrigueslima <vampleticiarodrigues@gmail.com>
2025-06-09 22:07:32 -0300

    Aula.13.05.php

--- aula.13.05.php
+++ aula.13.05.php
@@ -1,33 +1,73 @@
 <?php
-class Caneta {
-    public $marca;
+
+class caneta{
     private $ponta;
     private $cor;
-    public $tipo;
-    public function setPonta($ponta){
-        $this->ponta = $ponta;
-    }
-    public function getPonta(){
+
+    public function setPonta($ponta) {
+        $this->ponta = $ponta;   
+    } 
+    public function getPonta() {
         return $this->ponta;
+        echo "Ponta da caneta: " . $this->ponta. "<br>";    
+    }
+    public function __construct ($ponta, $cor) {
+        $this->ponta = $ponta;
+        $this->cor = $cor;
+    }
+    public function setCor($cor) {
+        $this->ponta = $cor;    
+        echo "Ponta da cor: " . $this->cor. "<br>";    
+    } 
+    public function getCor() {
+        return $this->cor;
+    }
+    public function mostracaneta() {
+        echo"<br> ##### <br> Caneta: <br> Ponta:" . $this->ponta;
+        echo"<br> Cor: " . $this->cor;
+    }
+
+}
+
+$c1= new caneta(10, "preta");
+$c1->getPonta();
+$c1->getCor();
+$c1->getCor("cinza");
+$c1->getCor();
+$c1->mostracaneta();
+
+$c2 = new caneta(15, "vermelha");
+$c2 ->mostracaneta();
+class carro {
+  private $modelo;
+  private $cor;
+  private $ano;
+    public function setModelo($modelo){
+        $this->modelo = $modelo;
     }
     public function setCor($cor){
         $this->cor = $cor;
     }
+    public function setAno($ano){
+        $this->ano = $ano;
+    }
+    public function getModelo(){
+        echo "<br> ######## <br> Modelo: ", $this->modelo;
+    }
     public function getCor(){
-        return $this->cor;
+        echo "<br> Cor: ", $this->cor;
     }
-    public function setTipo($tipo){
-        $this->tipo = $tipo;
+    public function getAno(){
+        echo "<br> Ano: ", $this->ano;
     }
-    public function getTipo(){
-        return $this->tipo;
-    }
+    public function __construct($modelo, $cor, $ano){
+        $this->modelo = $modelo;
+        $this->cor = $cor;
+        $this->ano = $ano;
+    }  
 }
-$c1 = new Caneta();
-$c2 = new Caneta();
-$c1->marca = "BIC";
-$c1->setCor("Azul");
-print_r($c1);
-echo "A cor da caneta é: " . $c1->getCor() . "<br>"; 
-print_r($c2);
+$carro1 = new carro("Storm", "cinza", 2018);
+$carro1->getModelo();
+$carro1->getCor();
+$carro1->getAno();
 ?>
